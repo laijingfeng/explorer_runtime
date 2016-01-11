7ab2206db9e9273c0587c13c48f37e020430d15c
laijingfeng <337292616@qq.com>
2016-01-11 12:36:25 +0800

    工程结构重整

---
+++ Code/Tools/Util.cs
@@ -0,0 +1,163 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 工具
+/// </summary>
+public class Util
+{
+    #region 查找
+
+    /// <summary>
+    /// 查找
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="parent"></param>
+    /// <param name="name"></param>
+    /// <param name="includeInactive"></param>
+    /// <returns></returns>
+    public static GameObject FindGo<T>(GameObject parent, string name, bool includeInactive = true) where T : Component
+    {
+        if (parent == null)
+        {
+            return null;
+        }
+
+        T t = FindCo<T>(parent.transform, name, includeInactive);
+
+        return (t == null) ? null : t.gameObject;
+    }
+
+    /// <summary>
+    /// 查找
+    /// </summary>
+    /// <param name="parent"></param>
+    /// <param name="name"></param>
+    /// <param name="includeInactive"></param>
+    /// <returns></returns>
+    public static GameObject FindGo(GameObject parent, string name, bool includeInactive = true)
+    {
+        if (parent == null)
+        {
+            return null;
+        }
+
+        Transform t = FindCo<Transform>(parent.transform, name, includeInactive);
+
+        return (t == null) ? null : t.gameObject;
+    }
+
+    /// <summary>
+    /// 查找
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="parent"></param>
+    /// <param name="name"></param>
+    /// <param name="includeInactive"></param>
+    /// <returns></returns>
+    public static T FindCo<T>(GameObject parent, string name, bool includeInactive = true) where T : Component
+    {
+        if (parent == null)
+        {
+            return null;
+        }
+
+        return FindCo<T>(parent.transform, name, includeInactive);
+    }
+
+    /// <summary>
+    /// 查找
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="parent"></param>
+    /// <param name="name"></param>
+    /// <param name="includeInactive"></param>
+    /// <returns></returns>
+    public static T FindCo<T>(Transform parent, string name, bool includeInactive = true) where T : Component
+    {
+        if (parent == null)
+        {
+            return null;
+        }
+
+        foreach (T t in parent.GetComponentsInChildren<T>(includeInactive))
+        {
+            if (t.name.Equals(name))
+            {
+                return t;
+            }
+        }
+
+        return null;
+    }
+
+    #endregion
+
+    #region 删除
+
+    /// <summary>
+    /// 立即删除所有儿子结点
+    /// </summary>
+    /// <param name="go"></param>
+    public static void DestroyAllChildrenImmediate(GameObject go)
+    {
+        if (go == null)
+        {
+            return;
+        }
+
+        List<GameObject> list = new List<GameObject>();
+
+        for (int i = 0, imax = go.transform.childCount; i < imax; i++)
+        {
+            list.Add(go.transform.GetChild(i).gameObject);
+        }
+
+        foreach (GameObject g in list)
+        {
+            UnityEngine.Object.DestroyImmediate(g);
+        }
+        list.Clear();
+    }
+
+    /// <summary>
+    /// 删除所有儿子结点
+    /// </summary>
+    /// <param name="go"></param>
+    public static void DestroyAllChildren(GameObject go)
+    {
+        if (go == null)
+        {
+            return;
+        }
+
+        List<GameObject> list = new List<GameObject>();
+
+        for (int i = 0, imax = go.transform.childCount; i < imax; i++)
+        {
+            list.Add(go.transform.GetChild(i).gameObject);
+        }
+
+        foreach (GameObject g in list)
+        {
+            UnityEngine.Object.Destroy(g);
+        }
+        list.Clear();
+    }
+
+    /// <summary>
+    /// 删除所有儿子结点
+    /// </summary>
+    /// <param name="comp"></param>
+    public static void DestroyAllChildren(Component comp)
+    {
+        if (comp == null)
+        {
+            return;
+        }
+        DestroyAllChildren(comp.gameObject);
+    }
+
+    #endregion
+}
---
+++ Code/Windows/Singleton.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/// <summary>
+/// 单例类
+/// </summary>
+/// <typeparam name="T"></typeparam>
+[System.Reflection.Obfuscation(ApplyToMembers = true, Exclude = true, Feature = "renaming")]
+public class Singleton<T>
+{
+    /// <summary>
+    /// 单例
+    /// </summary>
+    private static T m_instance = default(T);
+
+    /// <summary>
+    /// 单例
+    /// </summary>
+    public static T Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = (T)Activator.CreateInstance(typeof(T), true);
+            }
+            return m_instance;
+        }
+    }
+}
+
+/// <summary>
+/// <para>单例Mono</para>
+/// <para>存在形式优先级如下：</para>
+/// <para>初始在Hierarchy</para>
+/// <para>有和脚本同名的悬挂点</para>
+/// <para>在finalFather上或其子孙上</para>
+/// <para>新挂在finalFather上</para>
+/// </summary>
+[System.Reflection.Obfuscation(ApplyToMembers = true, Exclude = true, Feature = "renaming")]
+public class SingletonMono<T> : MonoBehaviour where T : UnityEngine.Component
+{
+    /// <summary>
+    /// 单例
+    /// </summary>
+    private static T m_instance = default(T);
+
+    /// <summary>
+    /// 最后无法找到，在这个父节点挂载
+    /// </summary>
+    protected static string finalFather = "/Client";
+
+    public virtual void Awake()
+    {
+        //一来就在Hierarchy的active对象直接初始化
+        m_instance = (T)(System.Object)(this);
+    }
+
+    /// <summary>
+    /// 单例
+    /// </summary>
+    public static T Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                //找同名的GameObject
+                GameObject root = GameObject.Find((typeof(T).Name));
+
+                if (root != null)
+                {
+                    m_instance = root.GetComponent<T>();
+                }
+                else
+                {
+                    //找到finalFather
+                    root = GameObject.Find(finalFather);
+
+                    //没有finalFather则新建单例
+                    if (root == null)
+                    {
+                        root = new GameObject(typeof(T).Name);
+                    }
+
+                    //找脚本
+                    T[] list = root.GetComponentsInChildren<T>(true);
+
+                    if (list != null 
+                        && list.Length != 0)
+                    {
+                        m_instance = list[0];
+                    }
+                    else
+                    {
+                        m_instance = root.AddComponent<T>();
+                    }
+                }
+            }
+
+            return m_instance;
+        }
+    }
+}
